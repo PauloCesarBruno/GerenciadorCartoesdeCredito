b4a5812d2f56abe60a39643dfaf32a2fe9d1fdaf
Paulo Bruno <p_bruno001@hotmail.com>
2021-07-20 11:55:12 -0300

    Trabalhando no Projeto

--- GerenciadorCartoesCredito/Models/Cartao.cs
+++ GerenciadorCartoesCredito/Models/Cartao.cs
@@ -8,12 +8,16 @@ namespace GerenciadorCartoesCredito.Models
         public int CartaoId { get; set; }
 
         [Required(ErrorMessage ="Campo Obrigatorio !")]
+        [Display(Name ="Nome do Banco")]
         public string NomeBanco { get; set; }
 
         [Required(ErrorMessage ="Campo Obrigatorio !")]
+        [Display(Name ="Número do Cartão")]
         public string NumeroCartao { get; set; }
 
         [Required(ErrorMessage ="Campo Obrigatorio !")]
+        [Display(Name ="Limite de Crédito")]
+        [DataType(DataType.Currency)]
         public double Limite { get; set; }
 
         public ICollection<Gasto> Gastos { get; set; }
